7531e6957af9857523fe8282f43aa53da3428d89
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-29 16:23:51 +0900

    Add files via upload

---
+++ ff.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h> 
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void){
+	int rfd,n;
+	char buf[256];
+	off_t start, cur;
+	int count_n = 0;
+	
+	rfd = open("data2.txt",O_RDONLY);
+	if(rfd == -1){
+		perror("Open data2.txt");
+		exit(1);
+	}
+	start = lseek(rfd, 0, SEEK_SET);
+	n = read(rfd,buf, sizeof(buf));
+	for(n = 0; n <strlen(buf); n++){
+		if(buf[n]== 'a'){
+			count_n++;
+		}
+	}
+	buf[n] = '\0';
+	printf("count word =%d\n",count_n);
+	close(rfd);	
+	return 0;
+}

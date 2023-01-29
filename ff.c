ddfd15f600b21808bd8034780b8b038a5b40461d
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-01-29 16:25:17 +0900

    파일에서 검색하기

--- ff.c
+++ ff.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <fcntl.h> 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+# include <stdio.h>
+# include <fcntl.h> 
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 
 int main(void){
 	int rfd,n;
